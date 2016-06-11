f6169e69b5513e55117ca3a31b1ecb356e174a8f
Pascal Thomet <pthomet@ivsweb.com>
2016-06-11 13:48:06 +0200

    Added working example "example1"

---
+++ examples/build_example1.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+mkdir example1.build
+cd example1.build
+cmake ../example1
+make
+echo
+echo "The example was build into example1.build"
+echo "The test is inside example1.build/MyLibrary/MyLibrary_DocTest"
+echo "Here is its output:"
+echo
+./MyLibrary/MyLibrary_DocTest
